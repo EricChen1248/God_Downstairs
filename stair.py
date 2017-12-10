0ddbcf23f03031eae2821cb588c17059906a7bea
greensylvia <34061393+greensylvia@users.noreply.github.com>
2017-12-11 02:23:25 +0800

    create class Stair

---
+++ stair.py
@@ -0,0 +1,29 @@
+import random
+
+class Stair(width):
+    x = random.randint(0, width - 25)  #假設圖片的長度是25
+    y = 0
+    type_number = random.randint(0, 3)
+    if type_number == 0:
+        type = "general"
+        photo_link = "xxx.png"     #連結到不同樓梯的圖
+    elif type_number == 1:
+        type = "hurt"
+        photo_link = "xxx.png"
+    else:
+        type = "cloud"
+        photo_link = "xxx.png"
+
+    photo = GraphicDisplay(photo_link)
+    
+    def Update():
+        """be touched or not and its reaction"""
+        if x <= Person.x <= (x + 25):
+            if type = "cloud":
+                y += 10            #樓梯繼續上升
+            elif type = "general":
+                reaction = Person.General()
+            else:
+                reaction = Person.Hurt()
+        else:
+            y += 10
