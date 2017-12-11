67bd589795d8137d6f67bd018d990d04e595729a
greensylvia <34061393+greensylvia@users.noreply.github.com>
2017-12-11 10:51:02 +0800

    Add __init__() to the class

--- stair.py
+++ stair.py
@@ -1,21 +1,21 @@
 import random
 
-class Stair(width):
-    x = random.randint(0, width - 25)  #假設圖片的長度是25
-    y = 0
-    type_number = random.randint(0, 3)
-    if type_number == 0:
-        type = "general"
-        photo_link = "xxx.png"     #連結到不同樓梯的圖
-    elif type_number == 1:
-        type = "hurt"
-        photo_link = "xxx.png"
-    else:
-        type = "cloud"
-        photo_link = "xxx.png"
-
-    photo = GraphicDisplay(photo_link)
+class Stair:
     
+    def __init__(self, width):
+        x = random.randint(0, width - 25)  #假設圖片的長度是25
+        y = 0
+        type_number = random.randint(0, 2)
+        if type_number == 0:
+            type = "general"
+            photo_link = "xxx.png"     #連結到不同樓梯的圖
+        elif type_number == 1:
+            type = "hurt"
+            photo_link = "xxx.png"
+        else:
+            type = "cloud"
+            photo_link = "xxx.png"
+            
     def Update():
         """be touched or not and its reaction"""
         if x <= Person.x <= (x + 25):
@@ -27,3 +27,4 @@ class Stair(width):
                 reaction = Person.Hurt()
         else:
             y += 10
+    
