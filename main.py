56d6b7977604f574a29d203290cfeef15f808ac0
angel30632 <32104602+angel30632@users.noreply.github.com>
2017-12-11 14:39:27 +0800

    merge main

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ def Button(msg,x,y,w,h,ic,ac,action = None):
     #change color
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
         pygame.draw.rect(game_display, ac,(x,y,w,h))
-        
         if click[0] == 1 and action != None:
             action()
     else:
         pygame.draw.rect(game_display, ic,(x,y,w,h))
+
 
     small_font = pygame.font.Font("freesansbold.ttf",20)
     text_surf, text_rect = TextObjects(msg, small_font)
@@ -179,7 +179,6 @@ def GameStart():
 
 #LOOP(Logic of the game)
 def GameLoop():
-    global pause
 
     gameExit = False
     while not gameExit:
@@ -202,7 +201,6 @@ def GameLoop():
             #Press Space to Pause
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    pause = True
                     Paused()
 
 
