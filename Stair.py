0784834de35f2adcdb6ab02e27871197d1616657
Eric Chen <EricChen1248@gmail.com>
2017-12-14 09:58:09 +0800

    Updated stair with global width/height variable

--- Stair.py
+++ Stair.py
@@ -1,10 +1,12 @@
 import pygame
 import random
 
+width = 150
+height = 20
 class Stair:
     
     def __init__(self, main_width, count):
-        """attributes of stair"""        
+        """ Attributes of stair """        
         type_number = random.randint(0, 10)
         if 0 <= type_number <= 4:
             self.type = "general"
@@ -14,14 +16,14 @@ class Stair:
             self.type = "cloud"
 
         
-        self.width = 150   #假設圖片寬度是150
-        self.height = 20  #假設圖片長度是20
+        self.width = width      # 假設圖片寬度是150
+        self.height = height    # 假設圖片長度是20
         self.x = random.randint(0, main_width - self.width)  
         self.y = 640 - 75 * (8 - count)
         self.count = 0
             
     def Update(self, person):
-        """be touched or not and its reaction"""
-        self.y -= 2    #所有樓梯不斷上升
+        """ be touched or not and its reaction """
+        self.y -= 2             # 所有樓梯不斷上升
         if self.y + self.height > (person.y + person.height - 2) > self.y and self.x <= (person.x + person.width) <= (self.x + self.width + person.width):         
             person.HitStair(self)
